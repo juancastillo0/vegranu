f4cc742608a9b761acc40785a36efe6073f5b34c
Juan Manuel Castillo <42351046+juancastillo0@users.noreply.github.com>
2025-11-10 20:00:06 -0500

    delete ._ photos and add dirPhotos map to generate routes

--- scripts/generate_routes.dart
+++ scripts/generate_routes.dart
@@ -16,10 +16,35 @@ Future<void> main() async {
       }
     }
   }
+
+  final photosDir = Directory('web/images/photos');
+  final photosDirPath = photosDir.uri.toFilePath(windows: false);
+  final dirToPhotos = <String, List<String>>{};
+  final photosToDelete = <File>[];
+  await for (final d in photosDir.list(recursive: true)) {
+    final path = d.uri.toFilePath(windows: false);
+    final segments = path.substring(photosDirPath.length).split('/');
+    final name = segments.lastWhere((v) => v.isNotEmpty);
+    if (d is File && name.startsWith('._') && name.endsWith('.jpg'))
+      photosToDelete.add(d);
+    if (d is! File || name.startsWith(RegExp('[._]'))) continue;
+
+    dirToPhotos.update(segments.first, (s) => s, ifAbsent: () => []).add(name);
+  }
   print(dirToContent);
+  print(dirToPhotos);
+  print(photosToDelete);
+  for (final d in photosToDelete) {
+    await d.delete();
+  }
+  print('deleted $photosToDelete');
+
   File('lib/routes_map.dart').writeAsString('''
 const routesMap = {
 ${dirToContent.entries.map((e) => '"${e.key}": ["${e.value.join('", "')}"],').join('\n')}
+};
+const dirPhotos = {
+${dirToPhotos.entries.map((e) => '"${e.key}": ["${e.value.join('", "')}"],').join('\n')}
 };
 ''');
   await Process.run('fvm', ['dart', 'format', 'lib/routes_map.dart']);
